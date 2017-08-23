1812925da04b71d2df7d120c22b83b7bb391d50c
Lukasz Waszczak <waszczak.lukasz@gmail.com>
2017-08-23 15:54:50 +0200

    powinno dzialac

--- PeopleAvatary/js/app.js
+++ PeopleAvatary/js/app.js
@@ -1,60 +1,44 @@
-var app = angular.module('webapp', ['ngRoute']);
+var app = angular.module('webapp', []);
 
 app.controller('mainController', function($scope, $http) {
-	var baseAJAXUrl = 'http://localhost:3000/db';
-	
-
-		$scope.makeUser = function(id, name, lastname) {
-			return { id: id, name: name, lastname: lastname }
-		}
-	
-		
-		/*
-1. Pobierzmy wszystkie zasoby z bazy danych - /db
-    - zasoby, ktore zostaną zwrócone, przypiszmy do $scope.resources
-2. Przy generowaniu losowego użytkownika  (w forze wypełniającym $scope.users) wylosujmy dane (imię i nazwisko).
-    - najpierw ustalmy płeć użytkownika, którego chcemy wypełnić
-        > losujemy liczbe <0, 1>
-        > następnie przypisujemy do zmiennej var sex; wartość 'male' w przypadku 0, 'female' w przypadku 1
-        > gdy chciałbym wylosować imiona i nazwiska żeńskie:
-            - sex + '_name', sex + '_lastname'
-
-*/
-		$http({
-            url: baseAJAXUrl,
-            method: 'GET'
-        }).then(function(success) {
-            $scope.resources = success.data;
-            $scope.loadData('');
-            console.log($scope.resources);
-        }, function(error) {
-            console.error(error);
-        });
-
+    $scope.makeUser = function(name, lastname) {
+        return { name: name, lastname: lastname }
+    }
     
-        $scope.loadData = function(sex) {
-            if(['male', 'female'].indexOf(sex.toLowerCase()) == -1) {
-                var r = Math.round(Math.random()); // <0, 1>
-                sex = (r === 0) ? 'male' : 'female';
-                
-                // pobierz losowe imie
-                $scope.users = [];
-                
-                $scope.getRandomValueFromArray = function(obj) {
-                  // 1. Losuje liczbe od 0 do dlugosci tablicy
-                  var r = Math.floor(Math.random() * obj.length);
-                  // 2. Zwracam obiekt z tablicy znajdujacy sie pod wylosowanym indeksem.
-                  return obj[r];
-                }
-                
-                // pobierz losowe imie
-                var countNames = $scope.resources[sex + '_name'].length;
-                for(var i = 0; i < 10; i++) {
-                    var name = $scope.getRandomValueFromArray($scope.resources[sex + '_name']);
-                    var lastname = $scope.getRandomValueFromArray($scope.resources[sex + '_lastname']);
-					$scope.users.push($scope.makeUser( i + 1, name, lastname));
-                }
-            }
-        }
+    $scope.getRandomItemFromArray = function(array) {
+        var r = Math.round(Math.random() * (array.length - 1));
+        return array[r];
+    }
+    /* 1. Pobierzmy wszystkie zasoby z bazy danych - /db
+        - zasoby, ktore zostaną zwrócone, przypiszmy do $scope.resources */
+    $http({
+        method: 'GET',
+        url: 'http://localhost:3000/db'
+    }).then(function(success) {
+        $scope.resources = success.data;
+        $scope.loadData('');
+    }, function(error) {
+        
+    }); 
+    
+    $scope.loadData = function(sex) {
+		var gender;
+		$scope.users = [];
+		for(var i = 0; i < 10; i++) {
+			if(sex === '') {
+				// losujemy liczbe <0, 1>
+            	var r = Math.round(Math.random()); // <0, 1>
+            	// następnie przypisujemy do zmiennej var sex; wartość 'male' w przypadku 0, 'female' w przypadku 1
+            	sex = (r === 0) ? 'male' : 'female';
+			} else {
+				gender = sex;
+			}
 		
+            var name = $scope.getRandomItemFromArray($scope.resources[gender + '_name']);
+            var lastname = $scope.getRandomItemFromArray($scope.resources[gender + '_lastname']);
+            $scope.users.push($scope.makeUser(name, lastname));
+        }    
+    }
+    
+
 });
