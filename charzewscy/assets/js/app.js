b7ebe60d5bf4c9b6d53cace2a1a2bb30fdb30a8d
Lukasz Waszczak <waszczak.lukasz@gmail.com>
2017-09-01 15:44:24 +0200

    friday version Charzewscy

---
+++ charzewscy/assets/js/app.js
@@ -0,0 +1,15 @@
+// toggle class scroll 
+$(window).scroll(function() {
+    if($(this).scrollTop() > 50)
+    {
+        $('.navbar-trans').addClass('afterscroll');
+    } else
+    {
+        $('.navbar-trans').removeClass('afterscroll');
+    }  
+
+});
+
+//===card-flipper=====
+//document.querySelector(".card-flip").toggle("flip");
+
