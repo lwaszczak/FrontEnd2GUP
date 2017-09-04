4ef495cdb7a40ff8e2421f9442c20aead72f077b
Lukasz <waszczak.lukasz@gmail.com>
2017-09-04 23:09:46 +0200

    monday version Charzewscy

--- charzewscy/assets/js/app.js
+++ charzewscy/assets/js/app.js
@@ -13,3 +13,23 @@ $(window).scroll(function() {
 //===card-flipper=====
 //document.querySelector(".card-flip").toggle("flip");
 
+$(document).ready(function(){
+	$("#testimonial-slider").owlCarousel({
+		items:3,
+		itemsDesktop:[1199,2],
+		itemsDesktopSmall:[979,2],
+		itemsTablet:[768,2],
+		itemsMobile:[600,1],
+		pagination:true,
+		navigation:false,
+		navigationText:["",""],
+		slideSpeed:1000,
+		autoPlay:true,
+		
+		
+		loop:true,
+		margin:10,
+		autoplayTimeout:1000,
+		autoplayHoverPause:true
+	});
+});
